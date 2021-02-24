2de299a829f1aa15f639008966498456269d0bfa
unknown <aishwaryababu1212@gmail.com>
2021-02-24 23:26:38 +0530

    basic graph construction

--- app/src/main/java/com/example/firetopology/MainActivity.java
+++ app/src/main/java/com/example/firetopology/MainActivity.java
@@ -1,61 +1,64 @@
 package com.example.firetopology;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.*;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.*;
+import com.opencsv.CSVReader;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class MainActivity extends AppCompatActivity {
-    static void printGraph(ArrayList<ArrayList<Integer> > adj)
-    {
-        for (int i = 0; i < adj.size(); i++) {
-            System.out.println("\nVertex" + i);
-            for (int j = 0; j < adj.get(i).size(); j++) {
-                System.out.print(" -> "+adj.get(i).get(j));
+public class MainActivity extends AppCompatActivity{
+    static void printGraph(ArrayList<ArrayList<Integer>> adj) {
+        for(int i=0;i<adj.size();i++) {
+            Log.d("Vertex", Integer.toString(i));
+            for(int j=0; j < adj.get(i).size();j++) {
+                Log.d("Vertices","->"+adj.get(i).get(j));
             }
-            System.out.println();
-        }
+    }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("EAF","AFdv");
         try {
-            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\adith\\eclipse-workspace\\Lab 1\\src\\not_cd\\excelshit.csv"));
-            String line=br.readLine();
+            Log.d("2","xyz2");
+            InputStream is = getResources().openRawResource(R.raw.book1);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String line = br.readLine();
             ArrayList<String> nodes = new ArrayList<>();
-
-            int v=0;
-            while((line = br.readLine()) != null)
-            {
+            int v = 0;
+            while ((line = br.readLine()) != null) {
                 nodes.add(line);
-                v+=1;
-
+                v++;
             }
-            ArrayList <ArrayList <Integer>> graph = new ArrayList<>(v);
-            HashMap<String, Integer> map = new HashMap<>();
-            for(int i=0;i<v;i++)
-            {
+            ArrayList<ArrayList<Integer>> graph = new ArrayList<>(v);
+            HashMap<String,Integer> map = new HashMap<>();
+            for(int i=0; i<v; i++) {
                 graph.add(new ArrayList<Integer>());
-                map.put(nodes.get(i).split(",")[0],i);
+                map.put(nodes.get(i).split(",")[0], i);
             }
-            for(int i=0;i<v;i++)
-            {
+
+            for(int i=0; i<v;i++) {
                 String[] array = nodes.get(i).split(",");
-                graph.get(map.get(array[0])).add(map.get(array[1]));
-                graph.get(map.get(array[0])).add(map.get(array[2]));
+//                for (int ii =0; ii<array.length;ii++) {
+//                    Log.d("err2",array[ii]);
+//                }
+
+                graph.get(map.get(array[0])).add(map.get(array[7]));
+                graph.get(map.get(array[0])).add(map.get(array[8]));
             }
             printGraph(graph);
-
         }
-        catch(Exception e) {
-
+        catch (Exception e) {
+            Log.d("err",e.toString());
         }
 
     }
+
+
+
+
 }

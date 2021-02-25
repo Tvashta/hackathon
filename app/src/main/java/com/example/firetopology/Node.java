5841892be76728cf354246fcd92db44404102a82
Adithi Narayan <adithin2001@gmail.com>
2021-02-25 13:22:17 +0530

    Order list

--- app/src/main/java/com/example/firetopology/Node.java
+++ app/src/main/java/com/example/firetopology/Node.java
@@ -6,11 +6,15 @@ public class Node {
     private String MAC_Neighbour_A;
     private String MAC_Neighbour_B;
 
-    public Node(String mac) {
+    public Node(String mac, String ip) {
         this.MAC = mac;
+        this.IP = ip;
     }
 
     public String getMAC() {
         return this.MAC;
+    }
+    public String getIP() {
+        return this.IP;
     }
 }

ea5f01913b2e58ce296ee942c58c58750508e1ea
Adithi Narayan <adithin2001@gmail.com>
2021-02-25 13:08:33 +0530

    Click listeners

--- app/src/main/java/com/example/firetopology/MainActivity.java
+++ app/src/main/java/com/example/firetopology/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.firetopology;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
 
 import com.google.android.flexbox.FlexDirection;
 import com.google.android.flexbox.FlexWrap;
@@ -22,6 +27,7 @@ import static java.lang.Math.max;
 
 
 public class MainActivity extends AppCompatActivity {
+    static ArrayList<String> nodes = new ArrayList<>();
 
     public static class BiMap<K, V> {
         HashMap<K, V> map = new HashMap<>();
@@ -37,10 +43,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         K getKey(V v) {
-            return inversedMap.get(v);}
+            return inversedMap.get(v);
+        }
     }
 
     static ArrayList<Integer> order = new ArrayList<>();
+
+    static String getNode(int row) {
+        return nodes.get(row);
+    }
 
     static void dfs(boolean[] visited, int v, ArrayList<ArrayList<Integer>> graph) {
         visited[v] = true;
@@ -53,20 +64,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     ArrayList<Node> nodesList = new ArrayList<Node>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerview);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
         FlexboxLayoutManager layoutManager = new FlexboxLayoutManager(this);
         layoutManager.setFlexDirection(FlexDirection.ROW);
-        layoutManager.setFlexWrap(FlexWrap.WRAP_REVERSE);
+        layoutManager.setFlexWrap(FlexWrap.WRAP);
         layoutManager.setJustifyContent(JustifyContent.FLEX_START);
         recyclerView.setLayoutManager(layoutManager);
 
 
         try {
-            ArrayList<String> nodes = new ArrayList<String>();
             InputStream is = getResources().openRawResource(R.raw.book1);
             BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
             String line = br.readLine();
@@ -102,6 +113,26 @@ public class MainActivity extends AppCompatActivity {
 
             NodeAdapter nodeAdapter = new NodeAdapter(nodesList);
             recyclerView.setAdapter(nodeAdapter);
+            recyclerView.addOnItemTouchListener(new RecyclerTouchListener(this,
+                    recyclerView, new ClickListener() {
+                @Override
+                public void onClick(View view, final int position) {
+                    details dialogFragment = new details();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("row", String.valueOf(position));
+                    dialogFragment.setArguments(bundle);
+                    dialogFragment.show(getSupportFragmentManager(), "details");
+                    //Values are passing to activity & to fragment as well
+                    Toast.makeText(MainActivity.this, "Single Click on position        :" + position,
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onLongClick(View view, int position) {
+                    Toast.makeText(MainActivity.this, "Long press on position :" + position,
+                            Toast.LENGTH_LONG).show();
+                }
+            }));
 
         } catch (Exception e) {
             Log.d("Error", e.toString());
@@ -110,8 +141,56 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public static interface ClickListener {
+        public void onClick(View view, int position);
 
+        public void onLongClick(View view, int position);
+    }
 
+    class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        private ClickListener clicklistener;
+        private GestureDetector gestureDetector;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recycleView, final ClickListener clicklistener) {
+
+            this.clicklistener = clicklistener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recycleView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clicklistener != null) {
+                        clicklistener.onLongClick(child, recycleView.getChildAdapterPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clicklistener != null && gestureDetector.onTouchEvent(e)) {
+                clicklistener.onClick(child, rv.getChildAdapterPosition(child));
+            }
+
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
 
 }
 
